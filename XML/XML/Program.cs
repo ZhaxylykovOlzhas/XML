a4a096ad568f57995b785b345afb19f9465a6826
Олжас <40593443+ZhaxylykovOlzhas@users.noreply.github.com>
2018-11-04 17:55:42 +0600

    Add files via upload

---
+++ XML/XML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                XmlDocument xml = new XmlDocument();
+                xml.Load("C:\\Users\\Олжас\\Desktop\\XmlAndJson-master\\XmlJson\\XmlJson\\Students.xml");
+
+            foreach (XmlNode task in xml.DocumentElement.ChildNodes)
+                {
+                        foreach (XmlNode param in task.ChildNodes)
+                        {
+                            Console.WriteLine(param.InnerText);
+                        }
+                }
+                Console.ReadLine();
+        }
+    }
+}
---
+++ XML/XML/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int StudentYear { get; set; }
+        public double AverageRating{ get; set;}
+        public Student(string name, int age, int studentYear, double averageRating)
+        {
+            Name = name;
+            Age = age;
+            StudentYear = studentYear;
+            AverageRating = averageRating;
+        }
+    }
+}
